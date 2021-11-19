d9b7483c583fbf5bc4c60f25c6f5852b4c1ad4c5
Bart van Wijk <Bartvanwijkzk@gmail.com>
2021-11-19 10:41:36 +0100

    Navigatie
    Navigatie Aangemaakt

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ALA Project</title>
+    <title></title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                
-            </ul>
-        </nav>
-    </header>
+<header>
+    <section class="navigatie">
+        <article class="logo">
+            <a href="index.php"><img src="assets/images/logo.jpeg" alt="Logo"></a>
+        </article>
+        <article class="navigatiebalk">
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Reviews</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav> 
+        </article>  
+    </section>
+
+    <main>
+        
+    </main>
+</header>
 </body>
 </html>
