c7221700d9c28c6c4e14af26806d011a14cec97b
Bart van Wijk <Bartvanwijkzk@gmail.com>
2021-11-29 15:38:34 +0100

    Nieuwe Footer

--- footer.php
+++ footer.php
@@ -1,16 +1,29 @@
-<div class="footer-basic">
-        <footer>
-            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
-            <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Home</a></li>
-                <li class="list-inline-item"><a href="#">Services</a></li>
-                <li class="list-inline-item"><a href="#">About</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-            </ul>
-            <p class="copyright">Gamestars © 2021</p>
-        </footer>
-    </div>
+<div class="section-div">
+    <footer class="footer-distributed">
+		<div class="footer-right">
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+			</div>
+			<div class="footer-left">
+				<p class="footer-links">
+					<a class="link-1" href="#">Home</a>
+
+					<a href="#">Bart</a>
+
+					<a href="#">Riki</a>
+
+					<a href="#">Thomas</a>
+
+					<a href="#">Terms</a>
+
+					<a href="#">Services</a>
+				</p>
+				<p>Gamestars &copy; 2021</p>
+			</div>
+	</footer>
+</div>
     <script src="js/main.js"></script>
 </body>
 </html>
