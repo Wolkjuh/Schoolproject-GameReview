af9a9199cebda2afd2e36e4402112df468b2dafb
Thomasvbruchem <94454940+Thomasvbruchem@users.noreply.github.com>
2021-12-08 15:43:49 +0100

    Update merch.php

--- merch.php
+++ merch.php
@@ -3,9 +3,121 @@
 ?>
 
 <main>
+<img class="shirt1" src="images/Schermafbeelding 2021-12-01 154849.png" alt="" >
+<article class="artikel1">
 
+<h3>maat:</h3>
+<select name="Maten" id="Maten">
+<option value="XS">XS</option>
+<option value="S">S</option>
+<option value="Medium">Medium</option>
+<option value="L">Large</option>
+<option value="XL">XL</option>
+<option value="XXL">XXL</option>
+</select><br>
+<h3>Aantal:</h3>
+<select name="Maten" id="Maten">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="6">7</option>
+    <option value="6">8</option>
+    <option value="6">9</option>
+    <option value="6">10</option>
+    </select><br><br><h3>Prijs:</h3>
+<p>€14,99</p><br>
+<button><img src="images/image-removebg-preview (10).png" alt="" height="30px"></button>
+</article>
+<img class="trui1" src="images/Schermafbeelding 2021-12-01 155006.png" alt="foto van merch"></article>
+<article class="artikel2">
+
+<h3>maat:</h3>
+<select name="Maten" id="Maten">
+<option value="XS">XS</option>
+<option value="S">S</option>
+<option value="Medium">Medium</option>
+<option value="L">Large</option>
+<option value="XL">XL</option>
+<option value="XXL">XXL</option>
+</select><br>
+<h3>Aantal:</h3>
+<select name="Maten" id="Maten">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="6">7</option>
+    <option value="6">8</option>
+    <option value="6">9</option>
+    <option value="6">10</option>
+    </select><br><br><h3>Prijs:</h3>
+<p>€24,99</p><br>
+<button><img src="images/image-removebg-preview (10).png" alt="" height="30px"></button>
+</article>
+<img class="shirt2" src="images/Schermafbeelding 2021-12-01 155049.png" alt="foto van merch">
+<article class="artikel3">
     
-
+    <h3>maat:</h3>
+    <select name="Maten" id="Maten">
+    <option value="XS">XS</option>
+    <option value="S">S</option>
+    <option value="Medium">Medium</option>
+    <option value="L">Large</option>
+    <option value="XL">XL</option>
+    <option value="XXL">XXL</option>
+    </select><br>
+    <h3>Aantal:</h3>
+    <select name="Maten" id="Maten">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="6">7</option>
+        <option value="6">8</option>
+        <option value="6">9</option>
+        <option value="6">10</option>
+        </select><br><br><h3>Prijs:</h3>
+    <p>€14,99</p><br>
+    <button><img src="images/image-removebg-preview (10).png" alt="" height="30px"></button>
+</article>
+<img class="trui2" src="images/Schermafbeelding 2021-12-01 155111.png" alt="foto van merch">
+<article class="artikel4">
+   
+    <h3>maat:</h3>
+    <select name="Maten" id="Maten">
+    <option value="XS">XS</option>
+    <option value="S">S</option>
+    <option value="Medium">Medium</option>
+    <option value="L">Large</option>
+    <option value="XL">XL</option>
+    <option value="XXL">XXL</option>
+    </select><br>
+    <h3>Aantal:</h3>
+    <select name="Maten" id="Maten">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="6">7</option>
+        <option value="6">8</option>
+        <option value="6">9</option>
+        <option value="6">10</option>
+        </select><br><br><h3>Prijs:</h3>
+        <p>€24,99</p><br>
+        <button><img src="images/image-removebg-preview (10).png" alt="" height="30px"></button>
+    </article> 
+    <article>
+        <button class="winkelmandje"><img  src="images/image-removebg-preview (10).png" alt="" height="80px"></button>
+</article>  
 </main>
 
 <?php   
