5e8c2498ef6599f0c89ba0ef8821ea9e9da50f90
Thomasvbruchem <94454940+Thomasvbruchem@users.noreply.github.com>
2021-12-08 15:50:51 +0100

    Update merch.php

--- merch.php
+++ merch.php
@@ -1,3 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="C:\Users\thoma\OneDrive\Documenten\ala 2 merch\images\styles.css">
+</head>
+
 <?php
     include_once 'header.php';
 ?>
